ac6f7ea3dc8e0d10346e28cee9b85f7e67535389
MorReky <mormemir@yandex.ru>
2020-09-10 07:04:13 +0000

    Start project

---
+++ AppWstAcademy/AppWstAcademy/DataFilesApp/OdbConnectHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppWstAcademy.DataFilesApp
+{
+    class OdbConnectHelper
+    {
+        public static AppAcademyWSREntities entObj;
+    }
+}
---
+++ AppWstAcademy/AppWstAcademy/PageMain/PageLogin.xaml.cs
@@ -0,0 +1,34 @@
+﻿using AppWstAcademy.DataFilesApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppWstAcademy.PageMain
+{
+    /// <summary>
+    /// Логика взаимодействия для PageLogin.xaml
+    /// </summary>
+    public partial class PageLogin : Page
+    {
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnReg_Click(object sender, RoutedEventArgs e)
+        {
+            FrameApp.frmObj.Navigate(new PageRegistration());
+        }
+    }
+}
