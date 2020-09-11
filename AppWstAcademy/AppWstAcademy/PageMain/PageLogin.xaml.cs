9def9f9287cc914bcc6689cb3839136127dbbcd7
MorReky <mormemir@yandex.ru>
2020-09-11 08:27:29 +0000

    WS2021 Part One

--- AppWstAcademy/AppWstAcademy/PageMain/PageLogin.xaml.cs
+++ AppWstAcademy/AppWstAcademy/PageMain/PageLogin.xaml.cs
@@ -30,5 +30,51 @@ namespace AppWstAcademy.PageMain
         {
             FrameApp.frmObj.Navigate(new PageRegistration());
         }
+        /// <summary>
+        /// Авторизация пользователя
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userObj = OdbConnectHelper.entObj.User.FirstOrDefault(
+                    x => x.Login == TxbLogin.Text && x.Password == PsbPassword.Password);
+                if (userObj == null)
+                {
+                    MessageBox.Show("Такой пользователь не найден",
+                                        "Уведомление",
+                                        MessageBoxButton.OK,
+                                        MessageBoxImage.Information);
+                    FrameApp.frmObj.Navigate(new PageRegistration());
+                }
+                else
+                {
+                    switch (userObj.IdRole)
+                    {
+                        case 1:
+                            MessageBox.Show("Здравствуйте, ученик!",
+                "Уведомление",
+                MessageBoxButton.OK,
+                MessageBoxImage.Warning);
+                            break;
+                        case 2:
+                            MessageBox.Show("Здравствуйте,учитель!",
+                "Уведомление",
+                MessageBoxButton.OK,
+                MessageBoxImage.Warning);
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Критический сбой в работе приложения" + ex.Message,
+                    "Уведомление",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+            }
+        }
     }
 }
--- AppWstAcademy/AppWstAcademy/PageMain/PageRegistration.xaml.cs
+++ AppWstAcademy/AppWstAcademy/PageMain/PageRegistration.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppWstAcademy.DataFilesApp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,65 @@ namespace AppWstAcademy.PageMain
         public PageRegistration()
         {
             InitializeComponent();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            FrameApp.frmObj.GoBack();
+        }
+        /// <summary>
+        /// Кнопка создания пользователя 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            if (OdbConnectHelper.entObj.User.Count(x=>x.Login==TxbLogin.Text)>0)
+            {
+                MessageBox.Show("Такой пользователь уже есть", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            else
+            {
+                try
+                {
+                    User userObj = new User()
+                    {
+                        Login=TxbLogin.Text,
+                        Password=PsbPassword.Password,
+                        IdRole=1
+                    };
+                    OdbConnectHelper.entObj.User.Add(userObj);
+                    OdbConnectHelper.entObj.SaveChanges();
+                MessageBox.Show("Пользователь создан", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Ошибка работы приложения:" + ex.Message.ToString(), "Критический сбой работы", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+
+            }
+
+        }
+        /// <summary>
+        /// Сравниваем пароли
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PsbPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (TxbPassword.Text==PsbPassword.Password)
+            {
+                BtnCreate.IsEnabled = true;
+                PsbPassword.Background = Brushes.LightGreen;
+                PsbPassword.BorderBrush = Brushes.Green;
+            }
+            else
+            {
+                BtnCreate.IsEnabled = false;
+                PsbPassword.Background = Brushes.LightCoral;
+                PsbPassword.BorderBrush = Brushes.Red;
+            }
         }
     }
 }
